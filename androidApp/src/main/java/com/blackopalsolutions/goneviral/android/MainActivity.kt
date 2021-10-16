6b32882537355705272dd07b219292decf1414b5
Caleb Young <caleb.a.young@gmail.com>
2021-10-15 21:12:37 -0600

    Add a basic "Draw Card" button

--- androidApp/src/main/java/com/blackopalsolutions/goneviral/android/MainActivity.kt
+++ androidApp/src/main/java/com/blackopalsolutions/goneviral/android/MainActivity.kt
@@ -6,13 +6,23 @@ import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var numOfCardsInHand = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val drawCardBtn = findViewById<Button>(R.id.draw_card_button)
         val viewHandBtn = findViewById<Button>(R.id.view_hand_button)
+
+        drawCardBtn.setOnClickListener {
+            numOfCardsInHand++;
+            Toast.makeText(this@MainActivity, "You just drew a card!", Toast.LENGTH_LONG).show()
+        }
+
         viewHandBtn.setOnClickListener {
-            Toast.makeText(this@MainActivity, "TODO: show hand", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@MainActivity, "# of cards in hand: $numOfCardsInHand", Toast.LENGTH_LONG).show()
         }
     }
 }

2e1e50a293394e6e8638fd1f0a25ddb985b37e91
carterwonnacott <59665836+carterwonnacott@users.noreply.github.com>
2021-12-06 21:00:34 -0700

    Add basic non-back-end functionality to login activity

--- androidApp/src/main/java/com/blackopalsolutions/goneviral/android/LoginActivity.kt
+++ androidApp/src/main/java/com/blackopalsolutions/goneviral/android/LoginActivity.kt
@@ -1,12 +1,38 @@
 package com.blackopalsolutions.goneviral.android
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.blackopalsolutions.goneviral.model.request.IdRequest
+import kotlin.random.Random
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var username = findViewById<EditText>(R.id.inputUsername).text
+        var password = findViewById<EditText>(R.id.inputPassword).text
+
+        val loginBtn = findViewById<Button>(R.id.btnLogin)
+        val registerBtn = findViewById<Button>(R.id.btnCreateAccount)
+
+        loginBtn.setOnClickListener {
+            // add actual checking functionality
+            val intent = Intent(this, MainActivity::class.java)
+            // probably need to add username and/or other information to intent
+            startActivity(intent)
+        }
+
+        registerBtn.setOnClickListener {
+            // add actual registration functionality
+            val intent = Intent(this, MainActivity::class.java)
+            // probably need to add username and/or other information to intent
+            startActivity(intent)
+        }
+
     }
 }
